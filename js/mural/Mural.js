71700ac55919f84ee0435c17f57800653a12d8d0
HeitorRamos132 <heitorgpramos95@gmail.com>
2020-11-12 18:40:52 -0300

    updated

--- js/mural/Mural.js
+++ js/mural/Mural.js
@@ -1,24 +1,80 @@
-const Mural = (function(_render, Filtro){
-    "use strict"
-    let cartoes = []
-    const render = () => _render({cartoes: cartoes, filtro: Filtro.tagsETexto});
+const Mural = (function (_render, Filtro) {
+  "use strict";
+  let cartoes = setCartoes();
 
-    Filtro.on("filtrado", render)
-
-    function adiciona(cartao){
-        cartoes.push(cartao)
-        cartao.on("mudanca.**", render)
-        cartao.on("remocao", ()=>{
-            cartoes = cartoes.slice(0)
-            cartoes.splice(cartoes.indexOf(cartao),1)
-            render()
-        })
-        render()
-        return true
+  function setCartoes() {
+    let cartoesLocal = JSON.parse(localStorage.getItem(usuario));
+    if (cartoesLocal) {
+      return cartoesLocal.map(
+        (cartaoLocal) => new Cartao(cartaoLocal.conteudo, cartaoLocal.tipo)
+      );
+    } else {
+      return [];
     }
+  }
 
-    return Object.seal({
-        adiciona
-    })
+  cartoes.forEach((cartao) => {
+    preparaCartao(cartao);
+  });
 
-})(Mural_render, Filtro)
+  const render = () => _render({ cartoes: cartoes, filtro: Filtro.tagsETexto });
+  render();
+
+  Filtro.on("filtrado", render);
+
+  function preparaCartao(cartao) {
+    const urlsImgs = Cartao.pegaImagens(cartao);
+    const prefix = "https://cors-anywhere.herokuapp.com/";
+    urlsImgs.forEach((url) => {
+      fetch(prefix + url).then((resposta) => {
+        caches.open("ceep-imagens").then((cache) => {
+          cache.put(url, resposta);
+        });
+      });
+    });
+
+    cartao.on("mudanca.**", salvaCartoes);
+    cartao.on("remocao", () => {
+      cartoes = cartoes.slice(0);
+      cartoes.splice(cartoes.indexOf(cartao), 1);
+      salvaCartoes();
+      render();
+    });
+  }
+
+  function salvaCartoes() {
+    localStorage.setItem(
+      usuario,
+      JSON.stringify(
+        cartoes.map((cartao) => {
+          return { conteudo: cartao.conteudo, tipo: cartao.tipo };
+        })
+      )
+    );
+  }
+
+  login.on("login", () => {
+    cartoes = setCartoes();
+    render();
+  });
+  login.on("logout", () => {
+    cartoes = [];
+    render();
+  });
+
+  function adiciona(cartao) {
+    if (logado) {
+      cartoes.push(cartao);
+      salvaCartoes();
+      cartao.on("mudanca.**", render);
+      preparaCartao(cartao);
+      let listaImagens = Cartao.pegaImagens(cartao);
+      render();
+      return true;
+    }
+  }
+
+  return Object.seal({
+    adiciona,
+  });
+})(Mural_render, Filtro);
